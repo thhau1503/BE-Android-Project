f6d9e32327467d9e8baf4437ec1a2bef1081151c
thhau1503 <thanhhaudeb204@gmail.com>
2024-09-08 11:26:56 +0700

    feat: create crud for notification

--- server.js
+++ server.js
@@ -2,10 +2,19 @@ const express = require('express');
 const mongoose = require('mongoose');
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./config/swaggerConfig');
+const socketIo = require('socket.io');
+const http = require('http');
 const cors = require('cors')
 require('dotenv').config();
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+    cors: {
+        origin: "http://localhost:3000", // Đảm bảo rằng URL của client được phép kết nối
+        methods: ["GET", "POST"]
+    }
+});
 
 // Kết nối tới MongoDB
 const db = process.env.MONGO_URI;
@@ -23,9 +32,22 @@ app.use(cors())
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 const authRoutes = require('./routes/auth');
+const notificationRoutes = require('./routes/notification');
 app.use('/api/auth', authRoutes);
+app.use('/api/notification', notificationRoutes);
+
+// Thiết lập kết nối WebSocket
+io.on('connection', (socket) => {
+    console.log('New client connected');
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+});
+
+// Lưu trữ đối tượng io để sử dụng trong các controller
+app.set('socketio', io);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
