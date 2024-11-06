23bffc3c49f0ead4c6fe6f94fdbe0a4653da9b46
thhau1503 <thanhhaudeb204@gmail.com>
2024-11-06 15:09:26 +0700

    add api create user for admin

--- controllers/authController.js
+++ controllers/authController.js
@@ -266,3 +266,32 @@ exports.deleteUserById = async (req, res) => {
   }
 };
 
+//Tạo người dùng cho admin
+exports.adminCreateUser = async (req, res) => {
+  const { username, password, email, phone, address, user_role } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ msg: 'User already exists' });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    user = new User({
+      username,
+      password: hashedPassword,
+      email,
+      phone,
+      address,
+      user_role: user_role || 'User' 
+    });
+
+    await user.save();
+    res.json({ msg: 'User created successfully', user });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ msg: 'Server error' });
+  }
+};
